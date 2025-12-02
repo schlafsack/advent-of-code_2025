dbbecbc50b8253bb7e85ba86699681d76149797b
Tom Greasley <965075+schlafsack@users.noreply.github.com>
2025-12-02 01:54:19 +0000

    day 1

--- src/day_1.rs
+++ src/day_1.rs
@@ -19,9 +19,9 @@ pub fn day_1_task_2(start: i32, filename: &str) -> i32 {
         .map_while(Result::ok)
         .map(parse_movement)
         .fold((0, start), |(a, b), i| {
-            let n = (b + i).rem_euclid(100);
-            let n1 = (b + i).abs().div(100);
-            let n2 = (b != 0 && b + i <= 0) as i32;
+            let n = (b + i).rem_euclid(100); // new dial position
+            let n1 = (b + i).abs().div(100); // number of whole turns past 0
+            let n2 = (b != 0 && b + i <= 0) as i32; // number of partial turns past 0
             (a + n1 + n2 , n)
         }).0
 }
