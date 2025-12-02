8008f69cb0efc84900f228c5226341147b5167cf
Tom Greasley <965075+schlafsack@users.noreply.github.com>
2025-12-02 01:40:45 +0000

    day 1

---
+++ src/day_1.rs
@@ -0,0 +1,41 @@
+use std::ops::{Div};
+use crate::utils;
+
+pub fn day_1_task_1(start: i32, filename: &str) -> i32 {
+    utils::read_lines(filename)
+        .expect("Unable to open file for reading")
+        .map_while(Result::ok)
+        .map(parse_movement)
+        .fold((0, start), |(a, b), i| {
+            let n = (b + i).rem_euclid(100);
+            let n1 = (n == 0) as i32;
+            (a + n1 , n)
+        }).0
+}
+
+pub fn day_1_task_2(start: i32, filename: &str) -> i32 {
+    utils::read_lines(filename)
+        .expect("Unable to open file for reading")
+        .map_while(Result::ok)
+        .map(parse_movement)
+        .fold((0, start), |(a, b), i| {
+            let n = (b + i).rem_euclid(100);
+            let n1 = (b + i).abs().div(100);
+            let n2 = (b != 0 && b + i <= 0) as i32;
+            (a + n1 + n2 , n)
+        }).0
+}
+
+fn parse_movement(movement: String) -> i32 {
+    movement
+        .chars()
+        .map(|c| match c {
+            'L' => '-',
+            'R' => ' ',
+            _ => c,
+        })
+        .collect::<String>()
+        .trim()
+        .parse()
+        .expect("Unable to parse movement.")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod day_1;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    println!("day 1 task 2: {}", day_1::day_1_task_1(50,"./input/1.test.txt"));
+    println!("day 1 task 2: {}", day_1::day_1_task_2(50,"./input/1.test.txt"));
 }
